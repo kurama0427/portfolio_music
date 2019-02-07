9022db2fe55fd7d529ad42332df6bf85d13f9e6a
kuromi <kurodaiyakyupannda@gmail.com>
2019-02-07 12:32:03 +0000

    img test

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,11 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :region                => 'ap-northeast-1',     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => 'AKIAJXYG46J7STG354CQ',
+      :aws_secret_access_key => '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'
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  'kuromiportfolio
+    config.cache_storage = :fog'
   end
 end

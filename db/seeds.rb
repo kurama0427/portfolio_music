14943937251a23e3862460bd9d27f91b906ab883
kuromi <kurodaiyakyupannda@gmail.com>
2019-03-04 08:55:34 +0000

    Modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,26 @@
 User.create!(name:  "だいき",
              email: "aaaa@aaaa.com",
              password:              "taretare",
-             password_confirmation: "taretare")
+             password_confirmation: "taretare",
+             picture: open("#{Rails.root}/app/assets/images/19381_en_1.jpg"))
 
 User.create!(name:  "くろみ",
              email: "bbbb@bbbb.com",
              password:              "katakata",
-             password_confirmation: "katakata")
+             password_confirmation: "katakata",
+             picture: open("#{Rails.root}/app/assets/images/suiren.jpg"))
              
 daiki = User.find(1);
 daiki.microposts.create!(song_title: "半径30cmの中を知らない",
                         instrument_list: ["ドラム"],
                         content: "キメが多くて難しいです！")
+                        
+kuromi = User.find(2);
+kuromi.microposts.create!(song_title: "曇天",
+                        instrument_list: ["ギター"],
+                        content: "基本的なコードで弾くことができます。初心者にもおすすめできます。",
+                        beginner: "初心者にもおすすめ")
+                        
+
 
              
